4df4e3cadae243e514d0e1a98fe1b729e38e3a20
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-02-20 12:05:49 +0530

    Update 4B.cpp

--- 306 - Lab on C++/Exercise-04/4B.cpp
+++ 306 - Lab on C++/Exercise-04/4B.cpp
@@ -1,21 +1,30 @@
-#include <iostream>
-using namespace std;
 
-long long fact(int num) {
-    if(num <= 1) {
-        return 1;
-    }
+    
 
-    return num * fact(num-1);
-}
 
-int main() {
-    int num;
 
-    cout << "Enter the number : ";
-    cin >> num;
 
-    cout << "Factorial of " << num << " : " << fact(num);
 
-    return 0;
-}
+
+#include <iostream> 
+ using namespace std; 
+ 
+ int sumFact(long long f) {
+   return (f < 10) ? f : (f%10) + sumFact(f/10);
+ }
+  
+ long long fact(int num) {
+     return (num <= 1) ? 1 : num * fact(num-1); 
+ } 
+ 
+int main() { 
+     int num; 
+  
+     cout << "Enter the number : "; 
+     cin >> num; 
+  
+     cout << "Factorial of " << num << " : " << fact(num) << endl;
+     cout << "Sum of factorial is : " << sumFact(fact(num)) << endl;
+     
+     return 0; 
+ }
