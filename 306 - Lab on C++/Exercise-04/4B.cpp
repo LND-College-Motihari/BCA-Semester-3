0186015aeed56d8ba291fa860f2ef2d303b9d205
Ayushi Kumari <115365818+ayushik10@users.noreply.github.com>
2023-03-20 22:01:51 +0530

    Update 4B.cpp

--- 306 - Lab on C++/Exercise-04/4B.cpp
+++ 306 - Lab on C++/Exercise-04/4B.cpp
@@ -1,30 +1,45 @@
+#include <iostream>
+using namespace std;
 
+/*
+
+int sumFact(long long f) {
+    return (f < 10) ? f : (f%10) + sumFact(f/10);
+}
+  
+long long fact(int num) {
+    return (num <= 1) ? 1 : num * fact(num-1); 
+}
+
+*/
+
+int sumFact(long long f) {
+    if(f == 0) {
+        return 0;
+    }
     
-
-
-
-
-
-
-#include <iostream> 
- using namespace std; 
- 
- int sumFact(long long f) {
-   return (f < 10) ? f : (f%10) + sumFact(f/10);
- }
+    int digit = f % 10;
+    
+    return digit + sumFact(f/10);
+}
   
- long long fact(int num) {
-     return (num <= 1) ? 1 : num * fact(num-1); 
- } 
- 
+long long fact(int num) {
+    if(num <= 1) {
+        return 1;
+    }
+    
+    return num * fact(num-1); 
+}
+
+
 int main() { 
-     int num; 
+    int num; 
   
-     cout << "Enter the number : "; 
-     cin >> num; 
+    cout << "Enter the number : "; 
+    cin >> num; 
   
-     cout << "Factorial of " << num << " : " << fact(num) << endl;
-     cout << "Sum of factorial is : " << sumFact(fact(num)) << endl;
+    cout << "Factorial of " << num << " : " << fact(num) << endl;
+    cout << "Sum of factorial is : " << sumFact(fact(num)) << endl;
      
-     return 0; 
- }
+    return 0; 
+}
